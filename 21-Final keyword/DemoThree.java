4c8a11511fd9f88874a36dfaaa7f25f49aac461c
Kanishka <kanishkab2917@gmail.com>
2024-05-27 11:28:44 +0530

    final methods are inherited but cannot be overridden

---
+++ 21-Final keyword/DemoThree.java
@@ -0,0 +1,27 @@
+class Alpha{
+
+	final void m1(){
+		System.out.println("Inside m1");
+	}
+}
+
+class Beta extends Alpha{
+
+	int z;
+	/*void m1(){  //cannot be overridden
+		System.out.println("Inside m1 overridden");
+	}*/
+}
+
+
+class DemoThree{
+
+	public static void main(String[] args){
+
+		Alpha a = new Alpha();
+		a.m1();
+		Beta b = new Beta();
+		b.m1();//final methods are inherited but cannot be overridden
+
+	}
+}
