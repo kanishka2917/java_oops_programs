8ae6a52c5fe640fc738dc70718015d235549f973
Kanishka <kanishkab2917@gmail.com>
2024-05-27 12:12:59 +0530

    assinging static final variable through hardcoding

---
+++ 21-Final keyword/DemoTen.java
@@ -0,0 +1,13 @@
+class Alpha{
+	
+	final static int i = 5;//assinging static final variable through hardcoding
+
+}
+
+class DemoTen 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(Alpha.i);
+	}
+}
