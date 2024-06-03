201b584d8ce9544f3001bebd5d61fabaa6be8fcb
Kanishka <kanishkab2917@gmail.com>
2024-06-03 08:36:29 +0530

    Implementing interface directly

---
+++ 24-Interface/DemoFour.java
@@ -0,0 +1,33 @@
+interface Alpha{
+	
+	int p = 1;
+	void a1();
+	void a2();
+}
+
+interface Beta{
+	
+	int q = 2;
+	void b1();
+	void b2();
+}
+interface Ceta{
+	
+	int r = 3;
+	void c1();
+	void c2();
+}
+interface Delta{
+	
+	int s = 4;
+	void d1();
+	void d2();
+}
+
+
+class DemoFour{
+
+	public static void main(String args[]){
+
+	}
+}
---
+++ 24-Interface/DemoThree.java
@@ -0,0 +1,19 @@
+interface Alpha{
+	
+	void fun(int a);
+}
+interface Beta{
+	
+	void fun(int a);
+}
+class Zeta implements Alpha, Beta{
+	
+	public void fun(int a)
+	{
+	}
+}
+class DemoThree{
+
+	public static void main(String args[]){
+	}
+}
